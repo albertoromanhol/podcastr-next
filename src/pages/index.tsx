aa3e0a9b7ee1910df848f8e9c763ca5c715cdf33
Alberto <albertoromanhol@gmail.com>
2021-04-23 09:32:32 -0300

    Aula 04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
+import { GetStaticProps } from "next";
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
-import { GetStaticProps } from "next";
 import Link from "next/link";
 import Image from "next/image";
+
 import { api } from "../services/api";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import { PlayerContext } from "../contexts/PlayerContext";
 
 import styles from "./home.module.scss";
 
@@ -25,32 +28,35 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
         <ul>
-          {latestEpisodes.map((episodes) => {
+          {latestEpisodes.map((episode) => {
             return (
-              <li key={episodes.id}>
+              <li key={episode.id}>
                 <Image
                   width={192}
                   height={192}
                   objectFit="cover"
-                  src={episodes.thumbnail}
-                  alt={episodes.title}
+                  src={episode.thumbnail}
+                  alt={episode.title}
                 />
 
                 <div className={styles.episodeDetails}>
-                  <Link href={`/episodes/${episodes.id}`}>
-                    <a>{episodes.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
                   </Link>
-                  <p>{episodes.members}</p>
-                  <span>{episodes.publishedAt}</span>
-                  <span>{episodes.durationAsString}</span>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
@@ -86,9 +92,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                     />
                   </td>
                   <td>
-                  <Link href={`/episodes/${episodes.id}`}>
-                    <a>{episodes.title}</a>
-                  </Link>
+                    <Link href={`/episodes/${episodes.id}`}>
+                      <a>{episodes.title}</a>
+                    </Link>
                   </td>
                   <td>{episodes.members}</td>
                   <td style={{ width: 80 }}>{episodes.publishedAt}</td>
